a4a4d178340eedc3a49404c178cda84bb017f905
Lfw08 <liufw666@outlook.com>
2024-08-06 00:04:02 +0800

    update

---
+++ config.py
@@ -0,0 +1,95 @@
+#worlds["Survival-1"] = "C:/Users/liufw/AppData/Roaming/.minecraft/versions/1.12.2-Forge_14.23.5.2859/saves/Survival"
+worlds["terralith"]  = "/home/lfw/.config/hmcl/.minecraft/versions/1.20.1/saves/Terralith"
+worlds["GSMP-3.0"]  = "./GSMP"
+
+'''
+#Survival Render
+
+renders["survival-1-day-south"] = {
+    "world": "terralith",
+    "title": "Daytime",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+
+renders["survival-1-cave-south"] = {
+    "world": "terralith",
+    "title": "Caves",
+    "rendermode": cave,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+
+renders["survival-1-night-south"] = {
+    "world": "terralith",
+    "title": "Nighttime",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+
+
+renders["survival-1-end-south"] = {
+    "world": "terralith",
+    "title": "Ender",
+    "rendermode": smooth_lighting,
+    "dimension": "end",
+    "northdirection" : "lower-right",
+}
+
+renders["survival-1-nether-south"] = {
+    "world": "terralith",
+    "title": "Nether",
+    "rendermode": nether_smooth_lighting,
+    "dimension": "nether",
+}
+
+'''
+
+#GSMP Render
+
+renders["survival-1-day-south"] = {
+    "world": "GSMP-3.0",
+    "title": "Daytime",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+
+renders["survival-1-cave-south"] = {
+    "world": "GSMP-3.0",
+    "title": "Caves",
+    "rendermode": cave,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+
+renders["survival-1-night-south"] = {
+    "world": "GSMP-3.0",
+    "title": "Nighttime",
+    "rendermode": smooth_night,
+    "dimension": "overworld",
+    "northdirection" : "lower-right",
+}
+
+
+renders["survival-1-end-south"] = {
+    "world": "GSMP-3.0",
+    "title": "Ender",
+    "rendermode": smooth_lighting,
+    "dimension": "end",
+    "northdirection" : "lower-right",
+}
+
+renders["survival-1-nether-south"] = {
+    "world": "GSMP-3.0",
+    "title": "Nether",
+    "rendermode": nether_smooth_lighting,
+    "dimension": "nether",
+}
+
+
+
+outputdir="./"
+
